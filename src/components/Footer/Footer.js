591e1573f1de5877f67741718fb0cb306494bd87
harsh <harsh@WHOHARSH>
2024-02-21 12:05:32 +0530

    fix some forment

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,54 +10,55 @@ import instagramIcon from '../../assets/instagram.svg'
 export function Footer() {
 
     return(
-        <Container className="Footer">
-           <a href='https://HarshRaval.in' className='logo'>
-            <samp>www.harsh</samp>
-            <span>raval.in</span>
-           </a>
-           <div>
-            <p> 
-              This Website was mad with react <img src={reactIcon} alt='React' />
-            </p>
-           </div>
 
-           <div className='social-media'>
-           <a
-          href="https://www.linkedin.com/in/harsh-raval-92a7321a2/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={linkedin} alt="Linkedin" />
+      <Container className="Footer">
+        <a href='https://HarshRaval.in' className='logo'>
+          <samp>www.harsh</samp>
+          <span>raval.in</span>
         </a>
-        <a
-          href="https://github.com/H1617K/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={githubIcon} alt="GitHub" />
-        </a>
-        <a
-          href="https://api.whatsapp.com/send/?phone=%2B919104868884&text=Hello+Harsh"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={whatsapp} alt="Whatsapp" />
-        </a>
-        <a
-          href="https://web.telegram.org/a/#901894708"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={telegram} alt="telegram" />
-        </a>
-        <a
-          href="https://www.instagram.com/who.harsh._/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={instagramIcon} alt="Instagram" />
-        </a>
-           </div>
-        </Container>
+        <div>
+          <p> 
+            This Website was mad with react <img src={reactIcon} alt='React' />
+          </p>
+        </div>
+
+        <div className='social-media'>
+          <a
+            href="https://www.linkedin.com/in/harsh-raval-92a7321a2/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={linkedin} alt="Linkedin" />
+          </a>
+          <a
+            href="https://github.com/H1617K/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={githubIcon} alt="GitHub" />
+          </a>
+          <a
+            href="https://api.whatsapp.com/send/?phone=%2B919104868884&text=Hello+Harsh"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={whatsapp} alt="Whatsapp" />
+          </a>
+          <a
+            href="https://web.telegram.org/a/#901894708"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={telegram} alt="telegram" />
+          </a>
+          <a
+            href="https://www.instagram.com/who.harsh._/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={instagramIcon} alt="Instagram" />
+          </a>
+        </div>
+      </Container>
     )
 }
